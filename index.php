1da932e67911e4e40a668e780acf86618c9b4549
CaptAm <captam@gmail.com>
2017-12-16 18:04:19 +0200

    Veikianti versija

--- index.php
+++ index.php
@@ -1,6 +1,54 @@
 <?php
 
+const USERS="users.json";
 include (__DIR__ . '/vendor/autoload.php');
+
+// Užkrauname userius
+if (file_exists(USERS))
+{
+  $users = json_decode(file_get_contents(USERS),true);
+}
+
+// Testuojam Trivia
+$client = new GuzzleHttp\Client();
+$res = $client->get('https://opentdb.com/api.php?amount=1&difficulty=easy');
+// echo $res->getStatusCode();
+// "200"
+// echo $res->getHeader('content-type');
+// 'application/json; charset=utf8'
+//echo $res->getBody();
+// {"type":"User"...'
+//echo "<hr>";
+// $parse=json_decode($res->get(), true);
+
+ $parse=$res->json();
+ $question=$parse["results"]["0"];
+// Outputs the JSON decoded data
+
+/*
+echo "<pre>";
+print_r($question);
+echo "</pre>";
+
+Array
+(
+    [category] => Geography
+    [type] => multiple
+    [difficulty] => easy
+    [question] => Which nation claims ownership of Antarctica?
+    [correct_answer] => No one, but there are claims.
+    [incorrect_answers] => Array
+        (
+            [0] => United States of America
+            [1] => United Nations
+            [2] => Australia
+        )
+
+)
+*/
+
+
+// FB integracija nuo èia
 
 $access_token = 'EAACNqZA0hLBEBAPfGua5rezqGfldRhzWxLR6HdSXP9B98U6x8OZC8QIljXCL0jfrA3Leh1yxNFLm8NYKHEOgZBm1YXuRKjZCmbH649EwTOObJTzvT5D9QVCRMZCY3cZByWsNSWXlNFJN9hZCGRN4V1Gy7HbFRa0nFauhJ4XFZAcmigZDZD';
 $verify_token = 'TOKEN';
@@ -35,9 +83,124 @@ $data = [
     'recipient' => [
         'id' => $sender,
     ],
-    'message' => [
+/*    'message' => [
         'text' => 'You wrote: ' . $message,
+        'quick_replies' => [
+          [
+            "content_type" => "text",
+            "title" => "Atsakymas 1",
+            "payload" => "ATS1"
+          ],
+          [
+            "content_type" => "text",
+            "title" => "Atsakymas 2",
+            "payload" => "ATS2"
+          ]
+        ] 
     ]
+*/
 ];
 
+$logged_user="";
+foreach ($users as $user_id => $user_data)
+{
+  if ($user_data["sender"] == $sender)
+  { 
+    $logged_user=$user_id; 
+  }
+}
+
+if ($logged_user=="")
+{
+// tODO: I VIENA IRASA
+  $users[]["sender"]=$sender;
+  $users[]["correct"]=0;
+  $users[]["incorrect"]=0;
+
+  // Cia reikia $logged_user nustatyti i paskutini elementa
+  foreach ($users as $user_id => $user_data)
+  {
+    if ($user_data["sender"] == $sender)
+    { $logged_user=$user_id; }
+  }
+}
+else 
+{
+    if ($message==$users[$logged_user]["right_answer"])
+    {
+       $data["message"]["text"]="Teisingai!";
+       $users[$logged_user]["correct"]++;
+    }
+    else 
+    {
+       $data["message"]["text"]="Neteisingai :(";
+       $users[$logged_user]["incorrect"]++;
+    }
+    $response = $fb->post('/me/messages', $data, $access_token);
+
+    $data["message"]["text"]="Teisingai: ".$users[$logged_user]["correct"];
+    $response = $fb->post('/me/messages', $data, $access_token);
+
+    $data["message"]["text"]="Neteisingai: ".$users[$logged_user]["incorrect"];
+    $response = $fb->post('/me/messages', $data, $access_token);
+
+    $message='Gauti klausima';
+}
+
+if ($message=='Gauti klausima')
+{
+  $data["message"]["text"]=html_entity_decode($question["question"]);
+
+  $qreply["content_type"]="text";
+  $qreply["title"]=$question["correct_answer"];
+  $qreply["payload"]=$question["correct_answer"];
+  $data["message"]["quick_replies"][]=$qreply;
+  $users[$logged_user]["right_answer"]=$question["correct_answer"];
+
+  foreach ($question["incorrect_answers"] as $key => $value)
+  {
+    $qreply["content_type"]="text";
+    $qreply["title"]=$value;
+    $qreply["payload"]=$value;
+    $data["message"]["quick_replies"][]=$qreply;
+  }
+}
+else 
+{
+  $data["message"]["text"]="Pradedam zaisti?";
+  $qreply["content_type"]="text";
+  $qreply["title"]="Gauti klausima";
+  $qreply["payload"]="Klausimas";
+  $data["message"]["quick_replies"][]=$qreply;
+}
+
+/*
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+
+/*
+echo "<pre>";
+print_r($question);
+echo "</pre>";
+echo "<hr>";
+
+echo "Kategorija: ".$question["category"]."<br>";
+echo "Sunkumas: ".$question["difficulty"]."<br>";
+echo "Klausimas: <b>".$question["question"]."</b><br>";
+
+$questions[]=$question;
+echo("<pre>");
+print_r($users);
+echo("<hr>");
+*/
+
 $response = $fb->post('/me/messages', $data, $access_token);
+
+// Išsaugojame userius
+$fp = fopen(USERS, 'w');
+fwrite($fp, json_encode($users));
+fclose($fp);
+
+?>
